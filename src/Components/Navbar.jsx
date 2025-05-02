08a0952a87d7edb2e881e4f7c41bc585688e5655
SManzarAbbas01 <manzarabbas.bscsf22@iba-suk.edu.pk>
2025-05-02 10:52:58 -0700

    initial commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+
+      
+    </div>
+  )
+}
+
+export default Navbar
